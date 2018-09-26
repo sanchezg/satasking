35d0f350754362f6e6265bcc20b4fb04e302c029
Gonzalo <gonchi.sanchez@gmail.com>
2018-09-25 21:44:19 -0300

    Redesign GT and Sat as python classes

--- ground-station.py
+++ ground-station.py
@@ -15,70 +15,93 @@ EVENT_MASK = selectors.EVENT_READ | selectors.EVENT_WRITE
 logger = logging.getLogger(__name__)
 
 
-def run_server(host, port):
-    """Execute a socket server and binds to specified host and port.
+class GroundStation:
+    """Represent the ground station that will handle tasking to remote clients satellites.
 
-    Return the selector used to switch between sockets.
+    By default the GroundStation listen on port 65265. This behavior can be set with `port`
+    argument.
     """
-    sel = selectors.DefaultSelector()
-    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Avoid bind() exception: OSError: [Errno 48] Address already in use
-    lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    lsock.bind((host, port))
-    lsock.listen()
-    logger.info('Listening on {}'.format((host, port)))
-    lsock.setblocking(False)
-    sel.register(lsock, selectors.EVENT_READ, data=None)
-    return sel
 
+    def __init__(self, host=DEFAULT_HOST, port=DEFAULT_PORT):
+        self.host = host
+        self.port = port
+        self.selector = None
+        self.clients = []
 
-def connect_client(sel, sock):
-    """Accept and register a new socket client connection."""
-    conn, addr = sock.accept()  # Should be ready to read
-    logger.info('accepted connection from {}'.format(addr))
-    conn.setblocking(False)
-    sel.register(conn, selectors.EVENT_READ, data=None)
+    def init_server(self):
+        """Init and start the socket server.
 
+        Init a socket server and bind it to the specified host and port.
+        The socket is configured as non-blocking.
+        """
+        if self.selector is not None:
+            raise RuntimeError("init_server should be called once")
+        self.selector = selectors.DefaultSelector()
+        lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Avoid bind() exception: OSError: [Errno 48] Address already in use
+        lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        lsock.bind((self.host, self.port))
+        lsock.listen()
+        logger.info('Listening on {}'.format((self.host, self.port)))
+        lsock.setblocking(False)
+        self.selector.register(lsock, EVENT_MASK, data=None)
 
-def disconnect_client(sel, sock):
-    """Unregister a client."""
-    try:
-        sel.unregister(sock)
-        sock.close()
-    except Exception as e:
-        logger.error(f'error: selector.unregister() exception for',
-                     {repr(e)})
-    else:
-        sock = None  # delete reference for gc
-        logger.info("Unregistered client")
+    def connect_client(self, socket_fd):
+        conn, addr = socket_fd.accept()
+        logger.info('Accepted connection from {}'.format(addr))
+        conn.setblocking(False)
+        self.selector.register(conn, selectors.EVENT_READ, data=None)
+        self.clients.append(conn)
+
+    def disconnect_client(self, sock):
+        """Unregister a client."""
+        try:
+            self.selector.unregister(sock)
+        except Exception as e:
+            logger.error(f'Error disconnecting client %s' % repr(e))
+        else:
+            logger.info("Unregistered client from %s" % repr(sock.getpeername()))
+            sock.close()
+            sock = None  # delete reference for gc
+
+    def run(self):
+        """Execute main loop."""
+        if self.selector is None:
+            self.init_server()
+        # TODO: this loop should be executed in a separated thread
+        try:
+            while True:
+                events = self.selector.select(timeout=None)  # block untils there're any sockets ready for I/O
+                for key, mask in events:
+                    if key.data is None:
+                        # from listening socket, then accept connection
+                        try:
+                            self.connect_client(key.fileobj)
+                        except OSError:
+                            # Client socket disconnected
+                            self.disconnect_client(key.fileobj)
+                    else:
+                        pass  # Do nothing by now
+        except KeyboardInterrupt:
+            logger.info('caught keyboard interrupt, exiting')
+        finally:
+            self.shutdown()
+
+    def shutdown(self):
+        """Perform shutdown operations, like closing the selector and opened sockets."""
+        self.selector.close()
+
 
 @click.command()
 @click.option('--host', default=DEFAULT_HOST, help='Host where the socket server will listen.')
 @click.option('--port', default=DEFAULT_PORT, help='Port where the socket server will listen.')
-def server_loop(host, port):
-    """Execute server main loop listening for new connections or clients responses."""
-    sel = run_server(host, port)
-    try:
-        while True:
-            events = sel.select(timeout=None)  # block untils there're any sockets ready for I/O
-            for key, mask in events:
-                if key.data is None:
-                    # from listening socket, then accept connection
-                    try:
-                        connect_client(sel, key.fileobj)
-                    except OSError:
-                        # Socket disconnected
-                        disconnect_client(sel, key.fileobj)
-                else:
-                    pass  # Do nothing by now
-    except KeyboardInterrupt:
-        logger.info('caught keyboard interrupt, exiting')
-    finally:
-        sel.close()
+def main(host, port):
+    gs = GroundStation(host, port)
+    gs.run()
 
 
 if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
     logger.addHandler(handler)
-    server_loop()
+    main()
--- satellite.py
+++ satellite.py
@@ -15,15 +15,41 @@ EVENT_MASK = selectors.EVENT_READ | selectors.EVENT_WRITE
 logger = logging.getLogger(__name__)
 
 
-def connect_server(host, port):
-    sel = selectors.DefaultSelector()
-    addr = (host, port)
-    logger.info('Connected to {}'.format(addr))
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setblocking(False)
-    sock.connect_ex(addr)
-    sel.register(sock, EVENT_MASK, data=None)
-    return sel
+class Satellite:
+    def __init__(self, host, port, *resources):
+        self.host = host
+        self.port = port
+        self.selector = None
+        self.resources = resources
+
+    def init_client(self):
+        if self.selector is not None:
+            raise RuntimeError("init_client should be called once")
+        self.selector = selectors.DefaultSelector()
+        self._connect_server()
+
+    def _connect_server(self):
+        """Connect to specified server."""
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex((self.host, self.port))
+        logger.info('Connected to {}'.format((self.host, self.port)))
+        self.selector.register(sock, EVENT_MASK, data=None)
+
+    def run(self):
+        if self.selector is None:
+            self.init_client()
+        # TODO: Move to a separated block
+        try:
+            while True:
+                events = self.selector.select(timeout=None)
+                # Check for a socket being monitored to continue.
+                if not self.selector.get_map():
+                    break
+        except KeyboardInterrupt:
+            print('caught keyboard interrupt, exiting')
+        finally:
+            self.selector.close()
 
 
 @click.command()
@@ -31,23 +57,13 @@ def connect_server(host, port):
 @click.option('--port', default=DEFAULT_PORT, help='Port where the socket server will listen.')
 @click.option('--resources', '-r', multiple=True,
               help='Resource id that this satellite will handle.')
-def sate_loop(host, port, resources):
-    """Execute server main loop listening for new connections or clients responses."""
-    sel = connect_server(host, port)
-    try:
-        while True:
-            events = sel.select(timeout=None)
-            # Check for a socket being monitored to continue.
-            if not sel.get_map():
-                break
-    except KeyboardInterrupt:
-        print('caught keyboard interrupt, exiting')
-    finally:
-        sel.close()
+def main(host, port, resources):
+    sate = Satellite(host, port, resources)
+    sate.run()
 
 
 if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
     logger.addHandler(handler)
-    sate_loop()
+    main()
