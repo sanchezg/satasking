717b0a4e87bc0d3b03a7aec6f4f88a1b8fa2ad0d
Gonzalo <gonchi.sanchez@gmail.com>
2018-10-09 22:23:35 -0300

    Add verbose logger

--- satasking/satasking/settings.py
+++ satasking/satasking/settings.py
@@ -106,9 +106,20 @@ AUTH_PASSWORD_VALIDATORS = [
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
         },
     },
     'loggers': {
